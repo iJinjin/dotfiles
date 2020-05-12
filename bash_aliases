defa4dc61747d4909775dc098fb5af71bfe686de
Youngjin Jin <ijinjin@kaist.ac.kr>
2020-05-12 18:18:03 +0900

    add bash aliases

---
+++ bash_aliases
@@ -0,0 +1,11 @@
+# ~/.bash_aliases
+# Some custom bash aliases for bash
+
+# clear screen
+alias c='clear'
+
+# ls modifications
+alias lt='ls --human-readable --size -l -S --classify'
+
+# git related aliases
+alias gs='git status'
--- makesym.sh
+++ makesym.sh
@@ -4,23 +4,27 @@
 # create symlinks
 ###################
 
-dir=~/dotfiles                  # dotfiles directory
-backupdir=~/dotfiles_backup     # backup dotfiles directory
+DIR=~/dotfiles                  # dotfiles directory
+DIR_BACKUP=~/dotfiles_backup     # backup dotfiles directory
 
 # list of files to symlink
-files="vimrc bashrc"            
+files="vimrc bashrc bash_aliases"            
 
-# create a backup directory for old dotfiles
-echo -n "Creating $backupdir in home directory..."
-mkdir -p $backupdir
-echo "Done"
+# create a backup directory for old dotfiles if not present
+if [ ! -d "$DIR_BACKUP" ]; then
+    echo -n "Creating $DIR_BACKUP in home directory... "
+    mkdir -p $DIR_BACKUP
+    echo "Done"
+fi
 
 # move existing dotfiles in home to the backup directory
-cd $dir || return
+cd $DIR || return
 for file in $files; do
-    echo "Moving $file from home to $backupdir..."
-    mv ~/.$file $backupdir/
-    echo "Creating symlink to $file in home directory..."
-    ln -s $dir/$file ~/.$file
+    echo -n "Moving $file from home to $DIR_BACKUP... "
+    mv "$HOME/.$file" "$DIR_BACKUP/"
+    echo "Done"
+    echo -n "Creating symlink to $file in home directory... "
+    ln -s "$DIR/$file" "$HOME/.$file"
+    echo "Done"
 done
 
